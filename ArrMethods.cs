3d34867eb655261dfda8e618bd05b4775d717f9f
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-26 14:32:46 +0300

    HW task01 done

---
+++ ArrMethods.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp260823
+{
+    public class ArrMethods
+    {
+        //Метод для створення масиву з випадкових чисел.
+        public static int[] CreateArr(int size)
+        {
+            int[] arr = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rnd.Next(1, 100);
+            }
+            return arr;
+        }
+        //Метод для виведення масиву на екран.
+        public static void PrintArr(int[] arr)
+        {
+            Console.WriteLine("Масив випадкових значень:");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        //Метод для отримання усіх парних чисел у масиві;
+        public static void Even(int[] arr)
+        {
+            Console.WriteLine("Парні числа:");
+            foreach (var item in arr)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+        //Метод для отримання усіх непарних чисел у масиві;
+        public static void Odd(int[] arr)
+        {
+            Console.WriteLine("Непарні числа:");
+            foreach (var item in arr)
+            {
+                if (item % 2 != 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+        //Метод для отримання усіх простих чисел у масиві;
+        public static void Prime(int[] arr)
+        {
+            Console.WriteLine("Прості числа:");
+            foreach (var item in arr)
+            {
+                if (item == 1)
+                {
+                    Console.Write(item + " ");
+                }
+                else
+                {
+                    for (int i = 2; i < item; i++)
+                    {
+                        if (item % i == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write(item + " ");
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+        //Метод для отримання усіх чисел Фібоначчі в масиві.
+        public static void Fibonacci(int[] arr)
+        {
+            Console.WriteLine("Числа Фібоначчі:");
+            foreach (var item in arr)
+            {
+                int a = 0;
+                int b = 1;
+                int c = 0;
+                while (c < item)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    if (c == item)
+                    {
+                        Console.Write(item + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,34 @@ namespace ConsoleApp260823
 {
     internal class Program
     {
+//Завдання 1
+//Створіть набір методів для роботи з масивами:
+//- Метод для отримання усіх парних чисел у масиві;
+//- Метод для отримання усіх непарних чисел у масиві;
+//- Метод для отримання усіх простих чисел у масиві;
+//- Метод для отримання усіх чисел Фібоначчі в масиві.
+//Використовуйте механізми делегатів.
+
+        delegate void MyDelegate(int[] arr);
         static void Main(string[] args)
         {
-            Console.WriteLine("HW/Mod06/ex01\n");
+            Console.WriteLine("HW/Mod09/ex01\n");
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+            
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            MyDelegate myDelegate = ArrMethods.Even;
+            myDelegate += ArrMethods.Odd;
+            myDelegate += ArrMethods.Prime;
+            myDelegate += ArrMethods.Fibonacci;
+            myDelegate(arr);
+            Console.WriteLine();
+
+            Console.WriteLine("Другий варіант, зі створенням масиву випадкових чисел:");
+            Console.WriteLine();
+            int[] arr01 = ArrMethods.CreateArr(10);
+            ArrMethods.PrintArr(arr01);
+            myDelegate(arr01);
         }
     }
 }
