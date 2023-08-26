9eda90a1285313a254bf973c2e03bba7096e53c0
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-26 15:36:32 +0300

    HW task02 done

---
+++ MyMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp260823
+{
+    public class MyMethods
+    {
+        public static void ShowTime()
+        {
+            Console.WriteLine($"Час: {DateTime.Now.ToLongTimeString()}");
+        }
+        public static void ShowDate()
+        {
+            Console.WriteLine($"Дата: {DateTime.Now.ToShortDateString()}");
+        }
+        public static void ShowDayOfWeek()
+        {
+            Console.WriteLine($"День: {DateTime.Now.DayOfWeek}");
+        }
+        public static bool IsDate(string date)
+        {
+            DateTime dt;
+            if (DateTime.TryParse(date, out dt))
+                return true;
+            else
+                return false;
+        }
+        public static double ShowTriangleArea(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine($"Площа трикутника = {area}");
+            return area;
+        }
+        public static double ShowRectangleArea(double a, double b)
+        {
+            double area = a * b;
+            Console.WriteLine($"Площа прямокутника = {area}");
+            return area;
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,44 @@ namespace ConsoleApp260823
 {
     internal class Program
     {
+//Завдання 2
+//Створіть набір методів:
+//- Метод відображення поточного часу;
+//- Метод відображення поточної дати;
+//- Метод відображення поточного дня тижня;
+//- Метод для підрахунку площі трикутника;
+//- Метод для підрахунку площі прямокутника.
+//Для реалізації проєкту використовуйте делегати: Action,
+//Predicate, Func.
+
+        delegate void MyDelegate();
         static void Main(string[] args)
         {
-            Console.WriteLine("HW/Mod06/ex01\n");
+            Console.WriteLine("HW/Mod09/ex02\n");
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+
+            Action action = MyMethods.ShowTime;
+            action += MyMethods.ShowDate;
+            action += MyMethods.ShowDayOfWeek;
+            action.Invoke();
+            Console.WriteLine();
+
+            Predicate<string> predicate = MyMethods.IsDate;
+            Console.WriteLine(predicate("12.12"));
+            Console.WriteLine(predicate("12.12.2012"));
+            Console.WriteLine(predicate("якесь значення"));
+            Console.WriteLine();
+
+                     
+
+            Func<double, double, double, double> func = MyMethods.ShowTriangleArea;
+            func.Invoke(3, 4, 5);
+            Console.WriteLine();
+
+            Func<double, double, double> func2 = MyMethods.ShowRectangleArea;
+            func2.Invoke(3, 4);
+            Console.WriteLine(); 
         }
     }
 }
